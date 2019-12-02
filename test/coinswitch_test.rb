0026e3187a4f34b4550112c74314fe191b0246f5
Jason Crockett <JesusisLord33333@outlook.com>
2019-12-02 16:23:23 +0000

    added coinslist test

--- test/coinswitch_test.rb
+++ test/coinswitch_test.rb
@@ -1,11 +1,19 @@
 require "test_helper"
-
+require_relative '../lib/coinswitch'
 class CoinswitchTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Coinswitch::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_it_gets_a_coins_list
+    client = Coinswitch::Dynamic::DynamicClient.new
+    coins = client.coins
+    refute_empty coins
   end
+
+  def test_it_gets_orders_list
+    client = Coinswitch::Dynamic::DynamicClient.new
+    refute_empty client.order
+  end
+
 end

2da6537bbc86c947712bc5c389524c9b7d3f178a
Jason Crockett <JesusisLord33333@outlook.com>
2019-12-02 16:51:35 +0000

    added more tests.

--- test/coinswitch_test.rb
+++ test/coinswitch_test.rb
@@ -11,9 +11,33 @@ class CoinswitchTest < Minitest::Test
     refute_empty coins
   end
 
+  def test_it_gets_supported_coins
+    client = Coinswitch::Dynamic::DynamicClient.new
+    refute_empty client.pairs
+  end
+
+  def test_it_gets_a_rate
+    client = Coinswitch::Dynamic::DynamicClient.new
+    refute_empty client.rate
+  end
+
+  def test_it_places_an_order
+    client = Coinswitch::Dynamic::DynamicClient.new
+    client.order
+  end
+
   def test_it_gets_orders_list
     client = Coinswitch::Dynamic::DynamicClient.new
     refute_empty client.order
+  end
+
+  def test_it_gets_an_order
+    client = Coinswitch::Dynamic::DynamicClient.new
+    refute_empty client.orderid
+  end
+  def test_it_gets_a_bulk_rate
+    client = Coinswitch::Dynamic::DynamicClient.new
+    refute_empty client.bulk_rate
   end
 
 end

a0b6556c8b1e0882d2441b1615fbba003c11b9cb
Jason Crockett <JesusisLord33333@outlook.com>
2019-12-03 15:07:07 +0000

    filled in some test parameters.

--- test/coinswitch_test.rb
+++ test/coinswitch_test.rb
@@ -13,27 +13,27 @@ class CoinswitchTest < Minitest::Test
 
   def test_it_gets_supported_coins
     client = Coinswitch::Dynamic::DynamicClient.new
-    refute_empty client.pairs
+    refute_empty client.pairs('eth','btc')
   end
 
   def test_it_gets_a_rate
     client = Coinswitch::Dynamic::DynamicClient.new
-    refute_empty client.rate
+    refute_empty client.rate('eth','btc')
   end
 
   def test_it_places_an_order
     client = Coinswitch::Dynamic::DynamicClient.new
-    client.order
+    refute_empty client.order('eth','btc',0.5,'35hK24tcLEWcgNA4JxpvbkNkoAcDGqQPsP','1FeexV6bAHb8ybZjqQMjJrcCrHGW9sb6uF')
   end
 
   def test_it_gets_orders_list
     client = Coinswitch::Dynamic::DynamicClient.new
-    refute_empty client.order
+    refute_empty client.orders 1,100
   end
 
   def test_it_gets_an_order
     client = Coinswitch::Dynamic::DynamicClient.new
-    refute_empty client.orderid
+    refute_empty client.orderid('33333333-6c9e-4c53-9a6d-55e089aebd04')
   end
   def test_it_gets_a_bulk_rate
     client = Coinswitch::Dynamic::DynamicClient.new

d7c57fcaf34b9e02321b7784ede7b2e63c8b43ab
Jason Crockett <JesusisLord33333@outlook.com>
2019-12-03 14:18:10 +0000

    wrote coins,coins_list,pairs,rate functions.

--- lib/coinswitch.rb
+++ lib/coinswitch.rb
@@ -10,6 +10,8 @@ module Coinswitch
     class DynamicClient
       ##
       # This class is a request constructor for coinswitch. It is the client for the dynamic api endpoints.
+      include HTTParty
+      base_uri 'https://api.coinswitch.co/v2'
       def initialize(ip_address=Ipify.ip,api_key=Coinswitch::SANDBOX_API_KEY,sandbox=true)
         ##
         # This Initialises the client and populates the coins list
@@ -20,18 +22,41 @@ module Coinswitch
         @coins = coins
       end
 
+
       attr_accessor
+
+
 
       def coins
       ##
       #   This gets a list of coins that are available for swapping
-
+        @@response = self.class.get('/coins',headers:{'x-api-key':@api_key,'x-user-ip':@ip_address})
+        return JSON.parse @@response.body
       end
-      def pairs
 
+      def coins_list
+        ##
+        # Gets ONLY a list of symbols available for trade.
+        @@coins_list = []
+        @@coins = self.coins
+        @@tomap = @@coins['data']
+        @@tomap.map{|x| @@coins_list << x["symbol"] }
+        return @@coins_list
+      end
+
+      def pairs(depositCoin,destinationCoin)
+        @@response = self.class.post('/pairs',
+                                     headers:{'x-api-key':@api_key,'x-user-ip':@ip_address},
+                                     body:JSON.unparse({'depositCoin'=> depositCoin,
+                                                        'destinationCoin' => destinationCoin}))
+        return @@response.body
       end
       def rate
-
+        @@response = self.class.post('/rate',
+                                     headers:{'x-api-key':@api_key,'x-user-ip':@ip_address},
+                                     body:JSON.unparse({'depositCoin'=> depositCoin,'destinationCoin' => destinationCoin})
+                                    )
+        return @@response.body
       end
 
       def order
@@ -49,3 +74,6 @@ module Coinswitch
     end
   end
 end
+
+client = Coinswitch::Dynamic::DynamicClient.new
+puts client.pairs("btc","eth")
